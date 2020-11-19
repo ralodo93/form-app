e72e359b55e78bfa9733ce923ce79f195f5a5ada
ralodo93 <raullopezdominguez@gmail.com>
2020-11-19 18:29:12 +0100

    rename

--- form-app-angular/e2e/src/app.e2e-spec.ts
+++ form-app-angular/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('angular-form app is running!');
+    expect(await page.getTitleText()).toEqual('form-app-angular app is running!');
   });
 
   afterEach(async () => {
--- form-app-angular/src/app/app.component.spec.ts
+++ form-app-angular/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-form'`, () => {
+  it(`should have as title 'form-app-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-form');
+    expect(app.title).toEqual('form-app-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-form app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('form-app-angular app is running!');
   });
 });
